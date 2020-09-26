5c0b392d822de00ec7c8ad8becb30a6e47fce7d0
Deepesh Chaudhari <deepeshchaudhari0786@gmail.com>
2020-09-26 16:14:14 +0530

    Update cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -68,3 +68,21 @@ print(combinations_with_replacement_list)
 # [(1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 1, 1), (1, 2, 2), (1, 2, 3), (1, 2, 1), (1, 3, 3), (1, 3, 1), 
 # (1, 1, 1), (2, 2, 2), (2, 2, 3), (2, 2, 1), (2, 3, 3), (2, 3, 1), (2, 1, 1), (3, 3, 3), (3, 3, 1), 
 # (3, 1, 1), (1, 1, 1)]
+
+# load and export pickle data
+p = open(PickleFileName, "wb")
+pickle.dump(df, p)
+p.close()
+p  = open(PickleFileName,"rb")
+see = pickle.load(p)
+p.close()
+
+
+# load json address
+f =  open(path_to_json)
+data = json.load(f)
+f.close()
+# write/export json file
+with open(json_file_name+".json", 'w') as file:
+    json_string = json.dumps(dct, default=lambda o: o.__dict__, sort_keys=True, indent=2)
+    file.write(json_string)
