ab5c89eb906b89bede1f00fec0037afacb387cf4
Deepesh Chaudhari <deepeshchaudhari0786@gmail.com>
2020-10-09 12:20:59 +0530

    add binary number
    add binary number

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,5 +1,14 @@
 # python3 competitive cheatsheat
 
+#Add Binary Number
+binary1 = "0b100"
+binary2 = "0b110"
+
+integer_sum = int(binary1, 2) + int(binary2, 2)
+binary_sum = bin(integer_sum)
+print(binary_sum)
+
+
 # implement min and maxheap
 import heapq 
 L=[10,20,5,90,52]
